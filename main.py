3a751d277ad4431a098c91ff8d3d151b91c24a35
Jessica Felix <>
2022-08-05 20:12:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+'''
+Teste comentário 1
+Teste comentário 2
+Teste comentário 3
+Múltiplos comentários podem ser feitos entre os códigos utilizando 3 aspas
+'''
+
+# Comentário 1 - um único comentário pode ser feito utilizando hashtag no início
+
+# Função Print  
+
+print('olá')
+print('tudo bem?')
+print("que dia é hoje?")
+
+# Variáveis
+
+# Variável em número
+
+x = 2
+print(x)
+print(x + x)
+print (x * x + x)
+
+# Variável em texto
+y = 'olá'
+
+# Modificando o tipo de dados
+
+x = str(3) # = texto (string)
+y = int(4) # = número inteiro (integer)
+z = float(5) # = fração (float)
+
+print(x + x)
+print(y + y)
+print(z+z)
+
+# Praticando com strings e integers
+
+nome = 'Jessica'
+idade = 27
+cidade = 'São Paulo'
+
+print('A ' + nome + ' tem ' + str(idade) + ' anos de idade e mora na cidade de ' +  cidade)
+
+cidade = 'Islândia'
+idade = 30
+
+print('Eu pretendo viajar para a ' + cidade + ' quando fizer ' + str(idade))
